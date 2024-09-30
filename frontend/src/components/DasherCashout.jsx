7d75af81b117394d27610f16912d8c6e50bb7c02
Liden <lninyow@gmail.com>
2024-09-30 20:10:10 +0800

    wip dashercashouts

--- frontend/src/components/DasherCashout.jsx
+++ frontend/src/components/DasherCashout.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from "react";
-import "./css/ShopApplication.css";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUpload, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import Navbar from "./Navbar/Navbar";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "../utils/axiosConfig";
 import { useAuth } from "../utils/AuthContext";
+import axios from "../utils/axiosConfig";
+import "./css/ShopApplication.css";
 
 const DasherCashout = () => {
   const { currentUser } = useAuth();
@@ -13,6 +12,7 @@ const DasherCashout = () => {
   const [imageFile, setImageFile] = useState(null);
   const [dragOver, setDragOver] = useState(false);
   const [GCASHName, setGCASHName] = useState("");
+  const [show,setShow] = useState(false);
   const [GCASHNumber, setGCASHNumber] = useState("");
   const [wallet, setWallet] = useState(0);
   const [cashoutAmount, setCashoutAmount] = useState(0);
@@ -31,8 +31,7 @@ const DasherCashout = () => {
     setSelectedImage(""); // Reset selected image
 };
 
-  useEffect(() => {
-    const fetchDasherData = async () => {
+const fetchDasherData = async () => {
       try {
         const response = await axios.get(`/dashers/${currentUser.id}`);
         const data = response.data;
@@ -44,20 +43,23 @@ const DasherCashout = () => {
       }
     };
 
-    const fetchCashoutData = async () => {
-      try {
+  const fetchCashoutData = async () => {
+    try {
         const response = await axios.get(`/cashouts/${currentUser.id}`);
-        setCashout(response.data);
-      } catch (error) {
-        console.error("Error fetching cashout data:", error);
-      }
+        const data = response.data;
+        console.log("Cashout data:", data); 
+        setCashout(Array.isArray(data) ? data : [data]); // Ensure data is an array
+    } catch (error) {
+        console.error("Error fetching cashout data:", error); 
+    }
+};
 
-    };
-
+  useEffect(() => {
     fetchDasherData();
     fetchCashoutData();
   }, [currentUser]);
 
+ 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setImageFile(file);
@@ -187,185 +189,187 @@ const DasherCashout = () => {
     return `${month}/${day}/${year} ${hours}:${minutes} ${ampm}`;
 };
 
+
+const withdrawModal = () => {<div className="adl-body">
+    <div className="p-content-current">
+      <div className="p-card-current">
+        <div className="p-container">
+          <div className="p-content">
+            <div className="p-text">
+              <h3>Withdraw Wallet</h3>
+              <h4>
+                It may take up to 3-5 business days for the amount to be reflected in your GCASH account.
+              </h4>
+            </div>
+          </div>
+          <div className="p-info">
+            <form onSubmit={handleSubmit}>
+
+              <div className="p-two">
+                <div className="p-field-two">
+                  <div className="p-label-two">
+                    <h3>GCASH Name</h3>
+                    <input
+                      type="text"
+                      className="gcash-name"
+                      value={GCASHName}
+                      onChange={(e) => setGCASHName(e.target.value)}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="p-field-two">
+                  <div className="p-label-two">
+                    <h3>GCASH Number</h3>
+                    <div className="gcash-input-container">
+                      <span className="gcash-prefix">+63 </span>
+                      <input
+                        type="number"
+                        className="gcash-num"
+                        value={GCASHNumber}
+                        onChange={(e) => setGCASHNumber(e.target.value)}
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="p-two">
+                <div className="sa-upload">
+                  <div className="sa-label-upload">
+                    <h3>GCASH Personal QR Code</h3>
+                  </div>
+                  <div
+                    className={`sa-upload-container ${
+                      dragOver ? "drag-over" : ""
+                    }`}
+                    // style={{height: "400px", width: "250px"}}
+                    onDragOver={handleDragOver}
+                    onDragLeave={handleDragLeave}
+                    onDrop={handleDrop}
+                  >
+                    <label htmlFor="sa-govID" className="sa-drop-area">
+                      <input
+                        type="file"
+                        hidden
+                        id="sa-govID"
+                        className="sa-govID-input"
+                        onChange={handleFileChange}
+                      />
+                      <div className="sa-img-view">
+                        {uploadedImage ? (
+                          <img
+                            src={uploadedImage}
+                            alt="Uploaded"
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                              borderRadius: "20px",
+                              objectFit: "cover",
+                            }}
+                          />
+                        ) : (
+                          <>
+                            <FontAwesomeIcon
+                              icon={faUpload}
+                              className="sa-upload-icon"
+                            />
+                            <p>
+                              Drag and Drop or click here <br /> to upload
+                              image
+                            </p>
+                          </>
+                        )}
+                      </div>
+                    </label>
+                  </div>
+                </div>
+
+                <div className="sa-shop-categories">
+                <div className="p-label-two">
+                    <h3>Cashout Amount (Wallet: ₱{wallet})</h3>
+                    <div className="gcash-input-container">
+                      <input
+                        type="number"
+                        className="gcash-num"
+                        value={cashoutAmount}
+                        onChange={(e) => setCashoutAmount(e.target.value)}
+                        max={wallet}
+                        required
+                      />
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+              <div className="p-buttons">
+                <button
+                  type="button"
+                  onClick={() => navigate("/profile")}
+                  className="p-logout-button"
+                >
+                  Cancel
+                </button>
+                <button type="submit" className="p-save-button">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+
+}
+
   return (
     <>
       
       {cashout && (
-      <>
-      <div className="adl-body">
-        <div className="adl-title">
-          <h2>You have a pending cashout request</h2>
-        </div>
-        <div className="adl-row-container">
-            <div className="adl-word">Timestamp</div>
-            <div className="adl-word">GCASH Name</div>
-            <div className="adl-word">GCASH Number</div>
-            <div className="adl-word">Amount</div>
-            <div className="adl-word">GCASH QR</div>
-            <div className="adl-word">Actions</div>
-        </div>
-
-        <div className="adl-container">
-                  
-          <div key={cashout.id} className="adl-box">
-              <div className="adl-box-content">
-                  <div>{formatDate(cashout.createdAt)}</div>
-                  <div>{cashout.gcashName}</div>
-                  <div>{cashout.gcashNumber}</div>
-                  {/* <div>₱{cashout.amount.toFixed(2)}</div> */}
-                  
-                  <div>
-                  <img 
-                          src={cashout.gcashQr} 
-                          alt="GCASH QR" 
-                          className="adl-list-pic" 
-                          onClick={() => handleImageClick(cashout.gcashQr)} // Click handler
-                      />
-                  </div>
-                  <div className="adl-buttons">
-                      <button className="adl-decline" onClick={() => handleDeleteClick(cashout.id)}>Delete</button>
-                      <button className="adl-acceptorder" onClick={() => handleEditClick(cashout.id)}>Edit</button>
-                  </div>
-              </div>
-          </div>
-                  
-        </div>
-        </div>
-      </>
-      )}
-
-
-      <div className="adl-body">
-        <div className="p-content-current">
-          <div className="p-card-current">
-            <div className="p-container">
-              <div className="p-content">
-                <div className="p-text">
-                  <h3>Withdraw Wallet</h3>
-                  <h4>
-                    It may take up to 3-5 business days for the amount to be reflected in your GCASH account.
-                  </h4>
+        <>
+            <div className="adl-body">
+                <div className="adl-title">
+                    <h2>You have a pending cashout request</h2>
                 </div>
-              </div>
-              <div className="p-info">
-                <form onSubmit={handleSubmit}>
+                <div className="adl-row-container">
+                    <div className="adl-word">Timestamp</div>
+                    <div className="adl-word">GCASH Name</div>
+                    <div className="adl-word">GCASH Number</div>
+                    <div className="adl-word">Amount</div>
+                    <div className="adl-word">GCASH QR</div>
+                    <div className="adl-word">Actions</div>
+                </div>
 
-                  <div className="p-two">
-                    <div className="p-field-two">
-                      <div className="p-label-two">
-                        <h3>GCASH Name</h3>
-                        <input
-                          type="text"
-                          className="gcash-name"
-                          value={GCASHName}
-                          onChange={(e) => setGCASHName(e.target.value)}
-                          required
-                        />
-                      </div>
-                    </div>
-                    <div className="p-field-two">
-                      <div className="p-label-two">
-                        <h3>GCASH Number</h3>
-                        <div className="gcash-input-container">
-                          <span className="gcash-prefix">+63 </span>
-                          <input
-                            type="number"
-                            className="gcash-num"
-                            value={GCASHNumber}
-                            onChange={(e) => setGCASHNumber(e.target.value)}
-                            required
-                          />
+                <div className="adl-container">
+                    {cashout.map((item) => (
+                        <div key={item.id} className="adl-box">
+                            <div className="adl-box-content">
+                                <div>{formatDate(item.createdAt)}</div>
+                                <div>{item.gcashName}</div>
+                                <div>{item.gcashNumber}</div>
+                                <div>₱{item.amount.toFixed(2)}</div>
+                                <div>
+                                    <img 
+                                        src={item.gcashQr} 
+                                        alt="GCASH QR" 
+                                        className="adl-list-pic" 
+                                        onClick={() => handleImageClick(item.gcashQr)} // Click handler
+                                    />
+                                </div>
+                                <div className="adl-buttons">
+                                    <button className="adl-decline" onClick={() => handleDeleteClick(item.id)}>Delete</button>
+                                    <button className="adl-acceptorder" onClick={() => handleEditClick(item.id)}>Edit</button>
+                                </div>
+                            </div>
                         </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="p-two">
-                    <div className="sa-upload">
-                      <div className="sa-label-upload">
-                        <h3>GCASH Personal QR Code</h3>
-                      </div>
-                      <div
-                        className={`sa-upload-container ${
-                          dragOver ? "drag-over" : ""
-                        }`}
-                        // style={{height: "400px", width: "250px"}}
-                        onDragOver={handleDragOver}
-                        onDragLeave={handleDragLeave}
-                        onDrop={handleDrop}
-                      >
-                        <label htmlFor="sa-govID" className="sa-drop-area">
-                          <input
-                            type="file"
-                            hidden
-                            id="sa-govID"
-                            className="sa-govID-input"
-                            onChange={handleFileChange}
-                          />
-                          <div className="sa-img-view">
-                            {uploadedImage ? (
-                              <img
-                                src={uploadedImage}
-                                alt="Uploaded"
-                                style={{
-                                  width: "100%",
-                                  height: "100%",
-                                  borderRadius: "20px",
-                                  objectFit: "cover",
-                                }}
-                              />
-                            ) : (
-                              <>
-                                <FontAwesomeIcon
-                                  icon={faUpload}
-                                  className="sa-upload-icon"
-                                />
-                                <p>
-                                  Drag and Drop or click here <br /> to upload
-                                  image
-                                </p>
-                              </>
-                            )}
-                          </div>
-                        </label>
-                      </div>
-                    </div>
-
-                    <div className="sa-shop-categories">
-                    <div className="p-label-two">
-                        <h3>Cashout Amount (Wallet: ₱{wallet})</h3>
-                        <div className="gcash-input-container">
-                          <input
-                            type="number"
-                            className="gcash-num"
-                            value={cashoutAmount}
-                            onChange={(e) => setCashoutAmount(e.target.value)}
-                            max={wallet}
-                            required
-                          />
-                        </div>
-                      </div>
-                    </div>
-                    
-                  </div>
-                  <div className="p-buttons">
-                    <button
-                      type="button"
-                      onClick={() => navigate("/profile")}
-                      className="p-logout-button"
-                    >
-                      Cancel
-                    </button>
-                    <button type="submit" className="p-save-button">
-                      Submit
-                    </button>
-                  </div>
-                </form>
-              </div>
+                    ))}
+                </div>
             </div>
-            
-          </div>
-        </div>
-      </div>
+        </>
+    )}
+
     </>
   );
 };
